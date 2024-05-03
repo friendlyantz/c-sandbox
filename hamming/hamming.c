5f20f325988fb0c10c773a63cd09f954a71f1478
anton <friendlyantz@pm.me>
2024-05-03 23:47:25 +1000

    Refac loop with pointers

--- hamming/hamming.c
+++ hamming/hamming.c
@@ -4,16 +4,7 @@
 
 int compute(const char *lhs, const char *rhs)
 {
-  size_t lhs_lnegth = strlen(lhs);
-  size_t rhs_lnegth = strlen(rhs);
-
-  if (lhs_lnegth != rhs_lnegth)
-  {
-    return -1;
-  }
-
   int distance = 0;
-  // for (int i = 0; i < (int)lhs_lnegth; i++)
   for (; *lhs != '\0' && *rhs != '\0'; lhs++, rhs++)
   {
     if (*lhs != *rhs)
@@ -21,5 +12,11 @@ int compute(const char *lhs, const char *rhs)
       distance++;
     }
   }
+
+  if (*lhs != '\0' || *rhs != '\0')
+  {
+    return -1;
+  }
+
   return distance;
 }
