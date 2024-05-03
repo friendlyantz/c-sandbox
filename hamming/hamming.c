6c36c7b285a6bf9768f5cfcf30005bc1bc4f8b82
anton <friendlyantz@pm.me>
2024-05-03 23:30:57 +1000

    REFAC: sizes

--- hamming/hamming.c
+++ hamming/hamming.c
@@ -4,8 +4,10 @@
 
 int compute(const char *lhs, const char *rhs)
 {
+  size_t lhs_lnegth = strlen(lhs);
+  size_t rhs_lnegth = strlen(rhs);
 
-  if (strlen(lhs) != strlen(rhs))
+  if (lhs_lnegth != rhs_lnegth)
   {
     return -1;
   }
