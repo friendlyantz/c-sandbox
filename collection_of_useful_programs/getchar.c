fb1732704076f6204200ce1c9d64a4420f84d748
anton <friendlyantz@pm.me>
2024-08-25 00:12:28 +1000

    format, fix typo, add comments

--- collection_of_useful_programs/getchar.c
+++ collection_of_useful_programs/getchar.c
@@ -1,19 +1,19 @@
-#import <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-  int lala; /* use int instead of char to be able to handle exit code */
+  int lala;     /* use int instead of char to be able to handle exit code */
   double count; /* long int */
 
   // count = 0;
   // while ((lala = getchar()) != EOF) {
-    // ++count; /* can be count++ */
-    // count++;
-    // putchar(lala);
-    // printf("%ld\n", count); // for int
+  // ++count; /* can be count++ */
+  // count++;
+  // putchar(lala);
+  // printf("%ld\n", count); // for int
   // }
 
-  for(count = 0; getchar() != EOF; ++count) {
-    printf("%.0f\n", count);
-  }
+  for (count = 0; getchar() != EOF; ++count)
+    ; // indicating that there is no body in for loope
+  printf("%.0f\n", count); // hit CTRL+D on Mac to send EOF signal
 }
