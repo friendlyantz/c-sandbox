d1e838a17a48bd40adc79384a672f7d6e9a538c7
anton <friendlyantz@pm.me>
2024-08-24 23:43:06 +1000

    use for loop

--- collection_of_useful_programs/getchar.c
+++ collection_of_useful_programs/getchar.c
@@ -3,13 +3,17 @@
 int main()
 {
   int lala; /* use int instead of char to be able to handle exit code */
-  long count; /* long int */
+  double count; /* long int */
 
-  count = 0;
-  while ((lala = getchar()) != EOF) {
-    ++count; /* can be count++ */
+  // count = 0;
+  // while ((lala = getchar()) != EOF) {
+    // ++count; /* can be count++ */
     // count++;
     // putchar(lala);
-    printf("%ld\n", count);
+    // printf("%ld\n", count); // for int
+  // }
+
+  for(count = 0; getchar() != EOF; ++count) {
+    printf("%.0f\n", count);
   }
 }
